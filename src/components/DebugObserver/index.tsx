0526de3aa0210460b9c148dc73a08cf79e879331
Lucas Dias <lf-dias@outlook.com>
2023-07-06 18:53:45 -0300

    Criando seletores async e componente para debugging

---
+++ src/components/DebugObserver/index.tsx
@@ -0,0 +1,16 @@
+import { useRecoilSnapshot } from "recoil";
+import { useEffect } from "react";
+
+function DebugObserver() {
+  const snapshot = useRecoilSnapshot();
+  useEffect(() => {
+    console.debug('The following atoms were modified:');
+    for (const node of snapshot.getNodes_UNSTABLE({ isModified: true })) {
+      console.debug(node.key, snapshot.getLoadable(node));
+    }
+  }, [snapshot]);
+
+  return null;
+}
+
+export default DebugObserver
--- src/state/atom.ts
+++ src/state/atom.ts
@@ -1,24 +1,10 @@
 import { atom } from "recoil";
 import { IEvento } from "../interfaces/IEvento";
 import { IFiltroDeEventos } from "../interfaces/IFiltroDeEventos";
+import { eventosAsync } from "./seletores";
 export const listaDeEventosState = atom<IEvento[]>({
   key: "listaDeEventosState",
-  default: [
-    {
-      descricao: "Estudar React",
-      inicio: new Date("2023-07-04T09:00"),
-      fim: new Date("2023-07-04T13:00"),
-      completo: false,
-      id: 1642342747,
-    },
-    {
-      descricao: "Estudar Recoil",
-      inicio: new Date("2023-07-05T09:00"),
-      fim: new Date("2023-07-05T11:00"),
-      completo: false,
-      id: 1642342959,
-    },
-  ],
+  default: eventosAsync,
 });
 
 export const filtroDeEventos = atom<IFiltroDeEventos>({
